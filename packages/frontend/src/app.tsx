d1be2d780313548a20af7319bc935b5d082cd8ed
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2023-02-17 15:11:54 +0100

    Handle suspense globally

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,9 +1,11 @@
 import { SSEProvider } from 'react-hooks-sse'
 import { Box } from '@mui/material'
+import React from 'react'
 import { DashTop } from './core/dash-top/dash-top'
 import { IotTabs } from './core/tabs/tabs'
 import { TabsProvider } from './core/tabs/tabs-context'
 import { SSEAtomHandler } from './core/data/sse/sse-atom-handler'
+import { SuspenseFallback } from './core/suspense-fallback'
 
 export function App() {
   return (
@@ -18,8 +20,10 @@ export function App() {
               width: '100vw',
             }}
           >
-            <DashTop />
-            <IotTabs />
+            <React.Suspense fallback={<SuspenseFallback />}>
+              <DashTop />
+              <IotTabs />
+            </React.Suspense>
           </Box>
         </TabsProvider>
       </SSEAtomHandler>
